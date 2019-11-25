45b01612eac093bb0cdf71ce50fabc2d948f9b5a
Iza IdeaHome <izaHomeIdea@gmail.com>
2019-11-25 22:23:45 +0100

    started  sushi list

--- src/index.js
+++ src/index.js
@@ -4,40 +4,50 @@ import { makis } from './app/data/makis';
 
 
 //   ****************   GENERAL   ***********************
-$('#app').addClass('container');
+const app = $('#app');
+console.log(app);
+app.addClass('container');
+app.css({ 'background-color': 'yellow'});
 
 
 
-//   ****************   HEADER   ************************
+// //   ****************   HEADER   ************************
+const header = $('<header></header>');
+const h2 =$('<h2>Promotion du jour</h2>');
+header.append(h2);
+app.append(header);
+header.css({ 'background-color': 'grey'});
 
 
+// //   ****************   SECTION   ***********************
 
-//   ****************   SECTION   ***********************
+// //   ---------  CREATE Liste MAKI ------------------
+// //TODO function
+const divSection = $('<div></div>');
+app.append(divSection);
+divSection.attr('id', 'divSection');
 
-//   ---------  CREATE Liste MAKI ------------------
-//TODO function
-const divSection = $('div');
-$('#app').append(divSection);
-divSection.attr('id','divSection');
-//  $('div').appendTo('#app').attr('id','divSection');
-//  $('div').appendTo('#divSection').attr('id','divMaki');
+// const divMaki = $('div');
+// divSection.append(divMaki);
+// divMaki.attr('id', 'divMaki');
 
     // fetch('assets/data/makis.json')
     // .then((reponse) => reponse.json())
     // .then((data) => {
     //     const makis = data.makis;
-        // for (let maki of makis){
-        // $('#divMaki').text(
-        //     `<figure>
-        //         <img src="${maki.image}" />
-        //     </figure>
-        //     <h3 id="description">${maki.description}</h3>
-        //     <h3 id="nom">${maki.nom}</h3>
-        //     <h3 id="prix">prix par piece : ${maki.prix}</h3>
-        //     <h3 id="prixBoite">prix par boite : ${maki.prix * 8}&euro;</h3>`
-        // );
+        for (let maki of makis){
+        $('#divSection').append(
+            `<figure>
+                <img src="${maki.image}" />
+            </figure>
+            <h3 id="description">${maki.description}</h3>
+            <h3 id="nom">${maki.nom}</h3>
+            <h3 id="prix">prix par piece : ${maki.prix}</h3>
+            <h3 id="prixBoite">prix par boite : ${maki.prix * 8}&euro;</h3>`
+        );
+        }
         //  }
-        // for (let maki of makis){
+        // for (const maki of makis) {
         // $('h3').appendTo('#divMaki').text(maki.description);
         // $('h3').appendTo('#divMaki').text(maki.nom);
         // $('h3').appendTo('#divMaki').text(maki.prix);
@@ -46,13 +56,7 @@ divSection.attr('id','divSection');
         // }
 
 
-
     // })
-
-
-
-
-
 
 
 //   ****************   FOOTER   ************************
